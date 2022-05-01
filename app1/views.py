8d521decfff873b49aedaeaf0535134c3a979afd
kf53495 <fkmndk902902@gmail.com>
2022-05-01 21:33:32 +0900

    indexのurlとviewの結びつけ

--- app1/views.py
+++ app1/views.py
@@ -1,4 +1,4 @@
-from django.views.generiv import TemplateView #htmlをそのまま返す
+from django.views.generic import TemplateView #htmlをそのまま返す
 
 class IndexView(TemplateView):
     template_name = 'index.html'
