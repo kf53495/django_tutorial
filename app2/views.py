9dd2c8ba2282ce2277a6df0bc1e4bb7c8d1d74de
kf53495 <fkmndk902902@gmail.com>
2022-05-02 17:06:48 +0900

    about2にget_context_dataを設定

--- app2/views.py
+++ app2/views.py
@@ -5,7 +5,13 @@ class Index2View(TemplateView):
 
     def get_context_data(self):
         ctxt = super().get_context_data() #superはスーパークラス(親クラス)がに関するオブジェクトを返す関数。ここでの親クラスはDjangoが用意しているTemplateViewなのでそこにある関数を使う
+        ctxt["username"] = "しゅんきち"
         return ctxt
 
 class About2View(TemplateView):
      template_name = 'about2.html'
+
+     def get_context_data(self):
+        ctxt = super().get_context_data()
+        ctxt["num_services"] = "1523"
+        return ctxt
