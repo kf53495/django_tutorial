9604c0457df599ef1e3b7d33bd354455b0faa392
kf53495 <fkmndk902902@gmail.com>
2022-05-02 16:57:22 +0900

    get_context_data関数を作成

--- app2/views.py
+++ app2/views.py
@@ -1,7 +1,11 @@
 from django.views.generic import TemplateView
 
 class Index2View(TemplateView):
-  template_name = 'index2.html'
+    template_name = 'index2.html'
+
+    def get_context_data(self):
+        ctxt = super().get_context_data() #superはスーパークラス(親クラス)がに関するオブジェクトを返す関数。ここでの親クラスはDjangoが用意しているTemplateViewなのでそこにある関数を使う
+        return ctxt
 
 class About2View(TemplateView):
-  template_name = 'about2.html'
+     template_name = 'about2.html'
