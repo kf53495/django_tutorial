a2a5d7bafbee9866d30aa694a75efa1f4a1ece34
kf53495 <fkmndk902902@gmail.com>
2022-05-02 14:51:17 +0900

    app2を呼び出せるように設定

--- app2/urls.py
+++ app2/urls.py
@@ -1,8 +1,8 @@
-from django.urls import path, include
+from django.urls import path
 
 from .views import Index2View, About2View
 
 urlpatterns = [
-    path('', Index2View.as_view())
-    path('about2/', About2View.as_view())
+    path('', Index2View.as_view()),
+    path('about2/', About2View.as_view()),
 ]
