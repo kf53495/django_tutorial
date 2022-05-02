5ebe50513bd39ec2c60c785a4b72e86afe3f36ab
kf53495 <fkmndk902902@gmail.com>
2022-05-02 15:57:22 +0900

    about2のurlを追加

--- config/urls.py
+++ config/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('app2.urls'))
+    path('', include('app2.urls')),
+    path('about/', include('app2.urls'))
 ]
